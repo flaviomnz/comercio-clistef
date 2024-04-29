4131afb45183257c1990e97fa398bbda0b6d354a
Flávio Batista dos Santos Menezes (Holding DTI) <DTI.FlavioB@aguiabranca.com.br>
2024-04-29 15:32:15 -0300

    alteracao um

--- main.py
+++ main.py
@@ -7,18 +7,28 @@ def ler_arquivo_ini(caminho_arquivo):
     config.read(caminho_arquivo)
     return config
 
+def extrair_informacoes(caminho_arquivo):
+    config = ler_arquivo_ini(caminho_arquivo)
+    informacoes = []
+    
+    campos = ['Empresa', 'Terminal', 'Operador', 'Porta']
+    info_secao = {'Arquivo': caminho_arquivo}
+    for secao in config.sections():
+        for campo in campos:
+            if config.has_option(secao, campo):
+                info_secao[campo] = config.get(secao, campo)
+        if len(info_secao) > 1:  # Se encontramos pelo menos um campo
+            informacoes.append(info_secao)
+    
+    return informacoes
+
 def percorrer_pastas(diretorio_raiz):
     dados = []
     for pasta_atual, _, arquivos in os.walk(diretorio_raiz):
         for arquivo in arquivos:
             if arquivo.lower().endswith('.ini'):
                 caminho_completo = os.path.join(pasta_atual, arquivo)
-                config = ler_arquivo_ini(caminho_completo)
-                
-                # Extrair todos os campos e valores do arquivo INI
-                for secao in config.sections():
-                    for chave, valor in config.items(secao):
-                        dados.append({'Arquivo': caminho_completo, 'Seção': secao, 'Chave': chave, 'Valor': valor})
+                dados.extend(extrair_informacoes(caminho_completo))
     
     # Criar um DataFrame com os dados
     df = pd.DataFrame(dados)
