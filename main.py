bea81988cefcacb6933a50f5e6b1496b1f896d32
Flávio Batista dos Santos Menezes (Holding DTI) <DTI.FlavioB@aguiabranca.com.br>
2024-07-09 11:52:32 -0300

    realease 0.0.3 again

--- main.py
+++ main.py
@@ -1,30 +1,45 @@
 import os
 import configparser
 import pandas as pd
+import logging
+from datetime import datetime
+
+# Configurando o logging
+log_filename = datetime.now().strftime('log_%Y-%m-%d_%H-%M-%S.log')
+logging.basicConfig(filename=log_filename, filemode='w', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def ler_arquivo_ini(caminho_arquivo):
-    config = configparser.ConfigParser()
-    config.read(caminho_arquivo)
-    return config
+    try:
+        config = configparser.ConfigParser()
+        config.read(caminho_arquivo)
+        return config
+    except Exception as e:
+        logging.error(f"Erro ao ler o arquivo INI: {caminho_arquivo} - {e}")
+        raise
 
 def extrair_informacoes(caminho_arquivo, nome_subpasta):
-    config = ler_arquivo_ini(caminho_arquivo)
-    informacoes = []
-    
-    campos = ['Empresa', 'Terminal', 'Operador', 'Porta', 'CNPJ', 'IP']
-    info_secao = {}
-    for secao in config.sections():
-        for campo in campos:
-            if config.has_option(secao, campo):
-                info_secao[campo] = config.get(secao, campo)
-        # Verificar se o campo "Operador" existe e, se não, adicionar o nome da subpasta
-        if 'Operador' not in info_secao or not info_secao['Operador']:
-            info_secao['Operador'] = nome_subpasta
+    try:
+        config = ler_arquivo_ini(caminho_arquivo)
+        informacoes = []
         
-        if len(info_secao) > 0:
-            informacoes.append(info_secao)
-    
-    return informacoes
+        campos = ['Empresa', 'Terminal', 'Operador', 'Porta', 'CNPJ', 'IP']
+        info_secao = {}
+        for secao in config.sections():
+            for campo in campos:
+                if config.has_option(secao, campo):
+                    info_secao[campo] = config.get(secao, campo)
+            # Verificar se o campo "Operador" existe e, se não, adicionar o nome da subpasta
+            if 'Operador' not in info_secao or not info_secao['Operador']:
+                info_secao['Operador'] = nome_subpasta
+            
+            if len(info_secao) > 0:
+                informacoes.append(info_secao)
+        
+        logging.info(f"Extraídas informações do arquivo: {caminho_arquivo}")
+        return informacoes
+    except Exception as e:
+        logging.error(f"Erro ao extrair informações do arquivo: {caminho_arquivo} - {e}")
+        return []
 
 def processar_pastas(diretorios_raiz):
     for pasta_raiz in diretorios_raiz:
@@ -40,11 +55,14 @@ def processar_pastas(diretorios_raiz):
         df = pd.DataFrame(dados).drop_duplicates()
         
         # Salvar em um arquivo Excel com formatação aprimorada
-        nome_arquivo_excel = f'dados_ini_{os.path.basename(pasta_raiz)}.xlsx'
-        df.to_excel(nome_arquivo_excel, index=False)
-        print(f"Dados extraídos e salvos em '{nome_arquivo_excel}'")
+        try:
+            nome_arquivo_excel = f'dados_ini_{os.path.basename(pasta_raiz)}.xlsx'
+            df.to_excel(nome_arquivo_excel, index=False)
+            logging.info(f"Dados extraídos e salvos em '{nome_arquivo_excel}'")
+        except Exception as e:
+            logging.error(f"Erro ao salvar os dados no arquivo Excel: {nome_arquivo_excel} - {e}")
 
-# Exemplo de uso com três diretórios raiz diferentes
+# Exemplo de uso com um diretório raiz
 diretorios_raiz = [
     r'C:\Users\flaviob\Desktop\Estrutura',
 ]
